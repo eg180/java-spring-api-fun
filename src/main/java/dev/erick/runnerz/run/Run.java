4d84f4bdafaca5c72beaf1c6b14857a256368701
Erick Gonzalez <ericksinbox@gmail.com>
2024-06-17 14:58:17 +0200

    Adds custom constraints and validation

--- src/main/java/dev/erick/runnerz/run/Run.java
+++ src/main/java/dev/erick/runnerz/run/Run.java
@@ -2,11 +2,26 @@ package dev.erick.runnerz.run;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 public record Run(
     Integer id,
+    @NotEmpty
     String title,
     LocalDateTime startedOn,
     LocalDateTime completedOn,
+    @Positive
     Integer miles,
     Location location
-) {}
+) {
+
+    public Run {
+        if (!completedOn.isAfter((startedOn))) {
+            throw new IllegalArgumentException("completedOn must be after startedOn");
+        }
+        // if (title.isEmpty()) {
+        //     throw new IllegalArgumentException("Title must be provided");
+        // }
+    }
+}
--- src/main/java/dev/erick/runnerz/run/RunController.java
+++ src/main/java/dev/erick/runnerz/run/RunController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/runs")
@@ -41,13 +43,13 @@ public class RunController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    void create(@RequestBody Run run) {
+    void create(@Valid @RequestBody Run run) {
         runRepository.create(run);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
         runRepository.update(run, id);
     }
 
